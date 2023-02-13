495d83a40383739678f74ebafba1146a4083d788
DaillanLeigh <francodaillanleigh@gmail.com>
2023-02-13 14:45:21 +0800

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+const Home = function () {
+  return (
+    <headers>
+      <nav>
+        <ul>
+          <li>About</li>
+          <li>Projects</li>
+          <li>Social</li>
+          <li>Contacts</li>
+        </ul>
+      </nav>
+    </headers>
+  );
+};
+
+export default Home;
