f711eba0979aba50dbdcaf1372c90a7c37a0206c
DaillanLeigh <francodaillanleigh@gmail.com>
2023-02-14 12:29:23 +0800

    Added skills link in navigation

--- components/Layouts/header.js
+++ components/Layouts/header.js
@@ -37,7 +37,18 @@ const Header = function () {
               Project
             </Link>
           </li>
-
+          <li>
+            <Link
+              to="skills"
+              spy={true}
+              smooth={true}
+              offset={50}
+              duration={500}
+              className="opacity-60 hover:opacity-100 cursor-pointer"
+            >
+              Skills
+            </Link>
+          </li>
           <li>
             <Link
               to="contact"
